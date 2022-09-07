1612f653618f192b0a604553693ee395f337cd72
LeipreachanTeo <pokiboom@mail.ru>
2022-09-07 08:09:15 +0700

    update

--- third_task/third_task.cs
+++ third_task/third_task.cs
@@ -5,12 +5,10 @@ int sum = 0;
 for(int i =0; i < array.Length; i++)
 {
     array[i] = new Random().Next(1 ,10);
-    Console.WriteLine(array[i]);
 }
 
 for(int i = 0; i <= array.Length/2 -1; i++)
 {
     sum += array[i] * array[array.Length - i - 1];
-    Console.WriteLine("+");
 }
 Console.WriteLine($"Сумма равна: {sum}");
