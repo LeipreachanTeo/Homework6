e7f6147bace2b40b259ab8751e8a9f4c8f24c4e7
LeipreachanTeo <pokiboom@mail.ru>
2022-09-07 07:08:14 +0700

    first task

---
+++ first_task/first_task.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите количество чисел которые нужно ввести: ");
+int M = Int16.Parse(Console.ReadLine());
+int [] array = new int[M];
+int i = 0;
+int count = 0;
+
+while(i < array.Length)
+{   
+    Console.WriteLine("Введите число: ");
+    array[i] = Int16.Parse(Console.ReadLine());
+    if (array[i] > 0)
+    {
+        count++;
+    }
+i++;
+}
+Console.Write("Количество чисел которые больше 0 : ");
+Console.WriteLine(count);
