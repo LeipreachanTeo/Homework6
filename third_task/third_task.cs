3ef5fa7d19af6b4ae7870c3ebed1b71b2a18a471
LeipreachanTeo <pokiboom@mail.ru>
2022-09-07 08:03:39 +0700

    third task

---
+++ third_task/third_task.cs
@@ -0,0 +1,16 @@
+﻿int N = new Random().Next(6,7);
+int[] array = new int[N];
+int sum = 0;
+
+for(int i =0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1 ,10);
+    Console.WriteLine(array[i]);
+}
+
+for(int i = 0; i <= array.Length/2 -1; i++)
+{
+    sum += array[i] * array[array.Length - i - 1];
+    Console.WriteLine("+");
+}
+Console.WriteLine($"Сумма ровна: {sum}");
